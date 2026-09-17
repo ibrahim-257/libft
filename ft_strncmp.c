0695e6b3501d24f06b373fa9552e36de8df1b247
ibrahim <ibrahimalnuman7@gmail.com>
2026-09-17 13:19:07 +0300

    fix: correct s1[1] to s1[i] typo in ft_strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialnuman <ibrahim.alnuman@learner.42.te    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/16 21:49:44 by ialnuman          #+#    #+#             */
+/*   Updated: 2026/09/16 21:49:44 by ialnuman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
