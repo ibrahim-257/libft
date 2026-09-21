3da5b0da1bb9b5fa2cc6c0a8a7f930a8de5a7977
ibrahim <ibrahimalnuman7@gmail.com>
2026-09-21 16:40:30 +0300

    feat: implement ft_putnbr_fd to output an integer to a file descriptor

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialnuman <ibrahim.alnuman@learner.42.te    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/21 16:31:42 by ialnuman          #+#    #+#             */
+/*   Updated: 2026/09/21 16:31:42 by ialnuman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	num;
+	char	c;
+
+	num = n;
+	if (num < 0)
+	{
+		write(fd, "-", 1);
+		num = -num;
+	}
+	if (num >= 10)
+		ft_putnbr_fd(num / 10, fd);
+	c = (num % 10) + '0';
+	write(fd, &c, 1);
+}
