aad1b640ac3e3988d5b73847be5983ea30198766
ibrahim <ibrahimalnuman7@gmail.com>
2026-09-21 16:20:17 +0300

    feat: implement ft_putstr_fd to output a string to a file descriptor

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -12,14 +12,14 @@
 
 #include "libft.h"
 
-void    ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-    int     i;
+	int	i;
 
-    if(!s)
-        return;
-    i = 0;
-    while(s[i])
-        i++;
-    write(fd, s, i);
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+		i++;
+	write(fd, s, i);
 }
