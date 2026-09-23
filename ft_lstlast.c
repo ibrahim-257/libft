64f66c8957b16a8bd90b84f068f7f691eba8bf28
ibrahim <ibrahimalnuman7@gmail.com>
2026-09-23 17:27:56 +0300

    feat: implement ft_lstlast to get the last node of a list and norm for ft_lstsize.c

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ialnuman <ibrahim.alnuman@learner.42.te    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/23 16:48:35 by ialnuman          #+#    #+#             */
-/*   Updated: 2026/09/23 16:48:35 by ialnuman         ###   ########.fr       */
+/*   Created: 2026/09/23 17:09:10 by ialnuman          #+#    #+#             */
+/*   Updated: 2026/09/23 17:09:10 by ialnuman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-unsigned int	ft_lstsize(t_list *lst)
+t_list	*ft_lstlast(t_list *lst)
 {
-	unsigned int	count;
-
-	count = 0;
-	while (lst != NULL)
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
 	{
 		lst = lst->next;
-		count++;
 	}
-	return (count);
+	return (lst);
 }
