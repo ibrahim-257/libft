0ff1d216e92bd3f53e09beba75be42e8ea180c96
ibrahim <ibrahimalnuman7@gmail.com>
2026-09-25 15:51:50 +0300

    feat: implement ft_lstiter to apply a function to each node's content

---
+++ ft_lstiter.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialnuman <ibrahim.alnuman@learner.42.te    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/24 17:58:47 by ialnuman          #+#    #+#             */
+/*   Updated: 2026/09/24 17:58:47 by ialnuman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst != NULL)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
